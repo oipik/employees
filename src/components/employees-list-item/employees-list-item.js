caaaf6b4594f27ce67da38880e82de510c827366
oipik <wazuup.lw@mail.ru>
2023-09-25 20:24:42 +0300

    add methods deleteItem, addItem, onToggleProp

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,54 +1,39 @@
-import { Component } from 'react';
-
 import './employees-list-item.css';
-import classnames from 'classnames';
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increse: false,
-            star: false
-        }
+const EmployeesListItem = (props) => {
+
+    const { name, salary, onDelete, onToggleProp, increase, rise } = props;
+
+    let classNames = "list-group-item d-flex justify-content-between";
+    if (increase) {
+        classNames += ' increase';
+    }
+    if (rise) {
+        classNames += ' like';
     }
 
-    onIncrease = () => {
-        this.setState(({ increase }) => ({
-            increase: !increase
-        }))
-    }
+    return (
+        <li className={classNames}>
+            <span onClick={onToggleProp} className="list-group-item-label" data-toggle="rise">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+            <div className='d-flex justify-content-center align-items-center'>
+                <button type="button"
+                    className="btn-cookie btn-sm "
+                    onClick={onToggleProp}
+                    data-toggle="increase">
+                    <i className="fas fa-cookie"></i>
+                </button>
 
-    addStar = () => {
-        this.setState(({ star }) => ({
-            star: !star
-        }))
-    }
+                <button type="button"
+                    className="btn-trash btn-sm "
+                    onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
 
-    render() {
-        const { name, salary } = this.props;
-        const { increase } = this.state;
-        const { star } = this.state;
-
-        return (
-            <li className={classnames('list-group-item d-flex justify-content-between', { 'increase': increase, 'like': star })}>
-                <span onClick={this.addStar} className="list-group-item-label">{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button type="button"
-                        className="btn-cookie btn-sm "
-                        onClick={this.onIncrease}>
-                        <i className="fas fa-cookie"></i>
-                    </button>
-
-                    <button type="button"
-                        className="btn-trash btn-sm ">
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        )
-    }
 }
 
 export default EmployeesListItem;
