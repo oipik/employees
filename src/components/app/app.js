5aa307e5a6b49ff817c91cc6dad2a28055bacddc
oipik <wazuup.lw@mail.ru>
2023-10-01 10:19:54 +0300

    Добавлен поиск и фильтры

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,14 +16,15 @@ class App extends Component {
                 { name: 'John C', salary: 800, increase: true, rise: true, id: 1 },
                 { name: 'Alex M.', salary: 3000, increase: false, rise: false, id: 2 },
                 { name: 'Carl W.', salary: 5000, increase: true, rise: false, id: 3 },
-            ]
+            ],
+            term: '',
+            filter: 'all',
         }
         this.maxId = 4;
     }
 
     deleteItem = (id) => {
         this.setState(({ data }) => {
-            // const index = data.findIndex(elem => elem.id == id);
             return {
                 data: data.filter(item => item.id !== id)
             }
@@ -50,7 +51,7 @@ class App extends Component {
     onToggleProp = (id, prop) => {
         this.setState(({data}) => ({
             data: data.map(item => {
-                if(item.id === id) {
+                if (item.id === id) {
                     return {...item, [prop]: !item[prop]}
                 }
                 return item;
@@ -58,22 +59,53 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThan1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
+
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter)
+        console.log(data);
 
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased} 
                     />
-
                 <div className="search-panel">
-                    <SearchPanel />
-                    <AppFilter />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
 
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp} />
                 <EmployeesAddForm 
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -2,7 +2,7 @@ import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
 
-    const { name, salary, onDelete, onToggleProp, increase, rise } = props;
+    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase) {
@@ -14,8 +14,8 @@ const EmployeesListItem = (props) => {
 
     return (
         <li className={classNames}>
-            <span onClick={onToggleProp} className="list-group-item-label" data-toggle="rise">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
+            <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm "
@@ -25,15 +25,14 @@ const EmployeesListItem = (props) => {
                 </button>
 
                 <button type="button"
-                    className="btn-trash btn-sm "
-                    onClick={onDelete}>
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}>
                     <i className="fas fa-trash"></i>
                 </button>
                 <i className="fas fa-star"></i>
             </div>
         </li>
     )
-
 }
 
 export default EmployeesListItem;
